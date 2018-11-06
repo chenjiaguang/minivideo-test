88d9eac704d2fe04481e77332161cfac17409947
chenjiayao <598423644@qq.com>
2018-11-06 20:03:35 +0800

    测试视频    chenjiaguang

--- pages/video/video.js
+++ pages/video/video.js
@@ -1,66 +1,109 @@
 // pages/video/video.js
+import util from '../../utils/util.js'
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    showFullScreen: true,
+    isFullScreen: false,
+    duration: 0,
+    current: 0,
+    currentTime: 0,
+    currentTimeText: '00:00',
+    maxTime: 0,
+    maxTimeText: '00:00',
+    percent: 0.0,
+    buffered: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad: function(options) {
+    console.log('onLoad')
+    setTimeout(() => {
+      let rData = {
+        limit: 20,
+        origin: "H5",
+        pn: 1,
+        snapshot: "",
+        token: "4baf1a75290740c4b8400a8a01bbaeca",
+        topicId: "20",
+        type: 0,
+      }
+      util.request('/jv/anonymous/qz/topic/find', rData).then(res => {
+        console.log('res', res)
+      }).catch(err => {
+        console.log('err', err)
+      })
+    }, 5000)
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-
+  onReady: function() {
+    this.videoContext = wx.createVideoContext('video-player')
   },
-
+  
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
+  },
+  touchmove: function() {
+    console.log('touchmove')
+  },
+  changeFullScreen: function () {
+    let {isFullScreen} = this.data
+    console.log('changeFullScreen', isFullScreen)
+    if (isFullScreen) {
+      this.videoContext.exitFullScreen()
+    } else {
+      this.videoContext.requestFullScreen()
+    }
+  },
+  onFullScreenChange: function (event) {
+    this.setData({
+      isFullScreen: event.detail.fullscreen
+    })
   }
 })
--- utils/util.js
+++ utils/util.js
@@ -1,19 +1,73 @@
-const formatTime = date => {
-  const year = date.getFullYear()
-  const month = date.getMonth() + 1
-  const day = date.getDate()
-  const hour = date.getHours()
-  const minute = date.getMinutes()
-  const second = date.getSeconds()
+const app = getApp()
 
-  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+const formatTime = date => {
+    const year = date.getFullYear()
+    const month = date.getMonth() + 1
+    const day = date.getDate()
+    const hour = date.getHours()
+    const minute = date.getMinutes()
+    const second = date.getSeconds()
+
+    return [year, month, day].map(formatNumber).join('-') + ' ' + [hour, minute, second].map(formatNumber).join(':')
 }
 
-const formatNumber = n => {
-  n = n.toString()
-  return n[1] ? n : '0' + n
+const formatNumber = n => { // 不足2位数的数字，自动在前面补0
+    n = n.toString()
+    return n[1] ? n : '0' + n
+}
+
+// 添加事件结束
+Promise.prototype.finally = function (callback) {
+    var Promise = this.constructor
+    return this.then(
+        function (value) {
+            Promise.resolve(callback()).then(
+                function () {
+                    return value
+                }
+            )
+        },
+        function (reason) {
+            Promise.resolve(callback()).then(
+                function () {
+                    throw reason
+                }
+            )
+        }
+    )
+}
+const request = (url, data, config) => {
+    return new Promise((resolve, reject) => {
+        wx.request({
+            url: app.globalData.baseUrl + url,
+            data: data,
+            method: (config && config.method) || 'POST', // 使用传入的method值，或者默认的post
+            header: {
+                'content-type': (config && config.contentType) || 'application/json', // 使用传入的contentType值，或者默认的application/json
+                'token': (config && config.token) || app.globalData.token || '', // 使用传入的token值，或者全局的token，都没有则默认空字符串
+            },
+            success: function (res) {
+                if (res.statusCode == 200) {
+                    resolve(res.data || res) // 返回成功提示信息
+                } else {
+                    reject(res.data || res) // 返回错误提示信息
+                }
+            },
+            fail: function (res) {
+                if (res && res.errMsg && res.errMsg === 'request:fail') { // 小程序请求失败，可以在这里检查是否联网，处理断网
+                    
+                }
+                reject(res) // 返回错误提示信息
+            },
+            complete: function (res) {
+
+            }
+        })
+    })
 }
 
 module.exports = {
-  formatTime: formatTime
+    formatTime: formatTime,
+    formatNumber: formatNumber,
+    request: request
 }
